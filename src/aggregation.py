2617aaf0b13e93b9ff070976abfcc7b99106431b
kevinzhai <zhaike12321@gmail.com>
2016-04-07 13:28:08 -0400

    unsorted aggregation

--- src/aggregation.py
+++ src/aggregation.py
@@ -5,7 +5,13 @@ import csv
 
 
 def make_dict(filename): 
-	truck_dict = [line.strip().split('\t') for line in open(filename).readlines()]
+	truck_dict = {}
+	for row in csv.DictReader(open(filename)) :
+		menu = {}
+		for items in row:
+			if row[items] != '':
+				menu[items] = row[items]
+		truck_dict[row['Truck Name']] = menu
 	return truck_dict
 
 if __name__ == '__main__' : 
